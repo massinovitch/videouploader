fb35d83b1573a23b475c23122312cd686d35e2b6
sekhi.massinissa@gmail.com <sekhi.massinissa@gmail.com@75924f39-2d3c-15df-10da-e649e1a42fdc>
2012-02-18 11:03:28 +0000

    msh : 1, 1. rajout de log 2. traitement des exception

--- trunk/VideoUploader.Ftp/Program.cs
+++ trunk/VideoUploader.Ftp/Program.cs
@@ -10,6 +10,13 @@ namespace VideoUploader.Ftp
         private static readonly ILog log = LogManager.GetLogger(typeof(Program));
         static void Main(string[] args)
         {
+            FTP ftp = new FTP()
+            {
+                Server = "192.168.1.15",
+                Username = "massi",
+                Password = "massi"
+            };
+            ftp.Download(@"massi.txt", @"c:\massi.txt");
             log.Info("massinissa");
         }
     }
