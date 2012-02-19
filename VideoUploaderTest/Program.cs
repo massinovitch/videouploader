409b23e02aa4c941ff23175d076cb15b8ead41f6
malekovitch@gmail.com <malekovitch@gmail.com@75924f39-2d3c-15df-10da-e649e1a42fdc>
2012-02-19 09:59:26 +0000

    smk : 1, exemple d execution de findByCriteria

--- VideoUploaderTest/Program.cs
+++ VideoUploaderTest/Program.cs
@@ -11,34 +11,36 @@ namespace VideoUploaderTest
     {
         static void Main(string[] args)
         {
+
             UserRepository userTest = new UserRepository();
-            
-            var user = new User
-            {
-                IdUser = 1,
-                Login = "massinovicth",
-                Nom = "sekhi",
-                Prenom = "massinissa",
-                GroupeIdGroupe = 1,
-                DateCreation = DateTime.Now,
-                DateMAJ = DateTime.Now,
-                Password = "pass"
-            };
-            userTest.Create(user);
-            
+
+            //var user = new User
+            //{
+            //    IdUser = 2,
+            //    Login = "massinovicth",
+            //    Nom = "sekhi",
+            //    Prenom = "massinissa",
+            //    GroupeIdGroupe = 1,
+            //    DateCreation = Convert.ToDateTime("01-01-2012"),
+            //    DateMAJ = DateTime.Now,
+            //    Password = "pass"
+            //};
+            //userTest.Create(user);
+            var userVide = new User();
             var userSearch = new User
             {
                 Login = "massinovicth",
                 Nom = "sekhi",
                 GroupeIdGroupe = 1
             };
-            List<User> userfind = userTest.FindByCriteria(userSearch);
+            EntitySearch entitySearch = new EntitySearch(userSearch, "2012-01-01", "");
+            List<User> userfind = userTest.FindByCriteria(entitySearch);
             foreach (User result in userfind)
             {
-                Console.WriteLine("User Last Name: {0}; First Name: {1}",
-                        result.Nom, result.Prenom);
+                Console.WriteLine("User Last Name: {0}; First Name: {1} ====== IdUser :: {2} ",
+                        result.Nom, result.Prenom, result.IdUser);
             }
             Console.ReadKey();
-        }
+    }
     }
 }
